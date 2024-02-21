25e9d54183a27624cbd6ae3900010e61cb6bada4
Raphaela Multhaup <raphaelamulthaup@icloud.com>
2024-02-21 06:45:02 +0100

    a-tag css and responsivnes

--- script.js
+++ script.js
@@ -11,15 +11,21 @@ async function render() {
   await getBundeslaender();
   for (let i = 0; i < responseAsJson.length; i++) {
     let bundesland = responseAsJson[i];
-      document.getElementById("main").innerHTML += divBundeslandhtml(bundesland);
+      document.getElementById("main").innerHTML += divBundeslandhtml(bundesland, i);
   }
 }
 
-function divBundeslandhtml(bundesland) {
+function divBundeslandhtml(bundesland, i) {
   return /*html*/ `
-        <a href="${bundesland['url']}">
-            <span>${bundesland['name']}</span>
-            <span>${bundesland['population']}</span>
+  <div class="whiteMargin">
+            <a id="${i}" href="${bundesland['url']}" onmouseover="hover(i)">
+            <span class="name">${bundesland['name']}</span>
+            <span class="population">${bundesland['population']} Millionen</span>
 </a>
+  </div>
     `;
+}
+
+function hover(i){
+    document.getElementById('i').classList.add('hover');
 }
